fbacb6760f01492fc50c521debd494c2f46d5c12
fomm1802 <106429671+fomm1802@users.noreply.github.com>
2023-05-20 08:37:32 +0700

    Update What Hub.lua

--- What Hub.lua
+++ What Hub.lua
@@ -1,21 +1,37 @@
 -- รายการ PlaceId และ URL สำหรับสคริปต์เกมแต่ละเกม
+
 local gameScripts = {
+
     [11542692507] = "https://raw.githubusercontent.com/bunnynwy/games/main/animesouls",
+
     [11430505281] = "https://raw.githubusercontent.com/bunnynwy/games/main/animeweapon",
+
     [6777872443] = "https://raw.githubusercontent.com/bunnynwy/games/main/pixelpiece",
+
     [13120948763] = "https://raw.githubusercontent.com/bunnynwy/games/main/animesmash_mobile",
+
     [12995840870] = "https://raw.githubusercontent.com/ToraIsMe/ToraIsMe/main/0strongman",
+
     [10704789056] = "https://raw.githubusercontent.com/BrickPlayz/Public-Scripts/main/DriveWorld.lua",
+
     -- เพิ่มเกมอื่นๆ ตามต้องการ
+
 }
 
 -- ตรวจสอบ PlaceId และโหลดสคริปต์เกมเฉพาะ
+
 local currentPlaceId = game.PlaceId
+
 local scriptURL = gameScripts[currentPlaceId]
 
 if scriptURL then
+
     loadstring(game:HttpGet(scriptURL))()
+
     print("โหลดสคริปต์เกมสำเร็จ")
+
 else
+
     print("เกมนี้ไม่ได้ใส่สคริปต์ไว้")
+
 end
