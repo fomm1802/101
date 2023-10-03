e01cfc028959204701d97c01ddff957f81450ab9
fomm1802 <106429671+fomm1802@users.noreply.github.com>
2023-10-03 20:23:58 +0700

    Update What Hub.lua

--- What Hub.lua
+++ What Hub.lua
@@ -1,13 +1,5 @@
 local gameScripts = {
-    [11542692507] = "https://raw.githubusercontent.com/bunnynwy/games/main/animesouls",
-    [11430505281] = "https://raw.githubusercontent.com/bunnynwy/games/main/animeweapon",
-    [6777872443] = "https://raw.githubusercontent.com/bunnynwy/games/main/pixelpiece",
-    [13120948763] = "https://raw.githubusercontent.com/bunnynwy/games/main/animesmash_mobile",
-    [12995840870] = "https://raw.githubusercontent.com/ToraIsMe/ToraIsMe/main/0strongman",
-    [10704789056] = "https://raw.githubusercontent.com/BrickPlayz/Public-Scripts/main/DriveWorld.lua",
-    [12985134424] = "https://raw.githubusercontent.com/ToraIsMe/ToraIsMe/main/0clickerfight",
-    [13234745978] = "https://raw.githubusercontent.com/ToraIsMe/ToraIsMe/main/0PunchaFriend",
-    [14364445098] = "https://raw.githubusercontent.com/Vernyfx/AnimeForce/main/Hi"
+    [14491001128] = "https://raw.githubusercontent.com/ToraIsMe/ToraIsMe/main/0Paragon"
 }
 
 -- ตรวจสอบ PlaceId และโหลดสคริปต์เกมเฉพาะ
