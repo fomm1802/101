9e506a053cd9e0b8c2c09c98d9673b57957b4ace
fomm1802 <106429671+fomm1802@users.noreply.github.com>
2023-10-04 13:27:13 +0700

    Update What Hub.lua

--- What Hub.lua
+++ What Hub.lua
@@ -1,6 +1,6 @@
 local gameScripts = {
     [14491001128] = "https://raw.githubusercontent.com/ToraIsMe/ToraIsMe/main/0Paragon",
-    [13772394625] = "https://raw.githubusercontent.com/1f0yt/community/main/RedCircleBlock"
+    [13772394625] = "https://api.luarmor.net/files/v3/loaders/8555f27454c952fb960ba00a4cd3f8f6.lua"
 }
 
 -- ตรวจสอบ PlaceId และโหลดสคริปต์เกมเฉพาะ
