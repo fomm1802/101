a76f017eb4cfcc21262899091f53c6c4dc228cc9
fomm1802 <106429671+fomm1802@users.noreply.github.com>
2024-07-11 18:48:18 +0700

    Update PS99.lua

--- PS99.lua
+++ PS99.lua
@@ -1,4 +1,5 @@
-
+loadstring(game:Http("https://raw.githubusercontent.com/fomm1802/101/main/AFK.lua"))()
+wait(5)
 while true do
     local args = {
         [1] = "cea0c738815e4f0cb680ad58ddf6a781",
