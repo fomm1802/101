810ac40e64f07a638d6aabeeb19da93416f66d88
fomm1802 <106429671+fomm1802@users.noreply.github.com>
2024-07-12 22:11:46 +0700

    Update PS99 V2.lua

--- PS99 V2.lua
+++ PS99 V2.lua
@@ -1,122 +1,104 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/WetCheezit/Bracket-V2/main/src.lua"))()
+local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt")()
 
--- Window
-local Window, MainGUI = Library:CreateWindow("Bracket-V2")
+local win = lib:Window("PREVIEW",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
 
--- Tabs
-local Tab1 = Window:CreateTab("Tab 1")
-local Tab2 = Window:CreateTab("Tab 2")
+-- Tab --
 
-local Groupbox1 = Tab1:CreateGroupbox("Groupbox 1", "Left")
-local Groupbox2 = Tab1:CreateGroupbox("Groupbox 2", "Right")
+local tab = win:Tab("Key")
 
-local Groupbox3 = Tab2:CreateGroupbox("Groupbox 1", "Left")
-local Groupbox4 = Tab2:CreateGroupbox("Groupbox 2", "Right")
-
--- Groupbox 1
-local ExampleToggle = Groupbox1:CreateToggle("Example toggle", function(state)
-   print(state)
+tab:Toggle("Use CrystalKey",false, function(t)
+   _G.CrystalKey = t
+   while _G.CrystalKey do wait(.1)
+      game:GetService("ReplicatedStorage").Network.CrystalKey_Unlock:InvokeServer()
+   end
 end)
 
-local ExampleToggle2 = Groupbox1:CreateToggle("Example keybind", function(state)
-   print(state)
+tab:Toggle("Use TechKey",false, function(t)
+   _G.TechKey = t
+   while _G.TechKey do wait(.1)
+      game:GetService("ReplicatedStorage").Network.TechKey_Unlock:InvokeServer()
+   end
 end)
 
-local ExampleButton = Groupbox1:CreateButton("Example Button", function()
-    print("Pressed")
+tab:Toggle("Use VoidKey",false, function(t)
+   _G.VoidKey = t
+   while _G.VoidKey do wait(.1)
+      game:GetService("ReplicatedStorage").Network.VoidKey_Unlock:InvokeServer()
+   end
 end)
 
-ExampleToggle2:CreateKeyBind()
+-- Tab2 --
 
-local ExampleSlider = Groupbox1:CreateSlider("Example slider", 0, 25, 0, function(value)
-   print(value)
+local tab2 = win:Tab("Wheel")
+
+tab:Toggle("Use StarterWheel",false, function(t)
+   _G.StarterWheel = t
+   while _G.StarterWheel do wait(.1)
+      local args = {
+      [1] = "StarterWheel"
+      }
+      game:GetService("ReplicatedStorage").Network:FindFirstChild("Spinny Wheel: Request Spin"):InvokeServer(unpack(args))
+   end
 end)
 
-local ExampleDropdown = Groupbox1:CreateDropdown("Example Dropdown", {"Option 1", "Option 2", "Option 3"}, function(state)
-   print(state)
+tab:Toggle("Use TechWheel",false, function(t)
+   _G.TechWheel = t
+   while _G.TechWheel do wait(.1)
+      local args = {
+      [1] = "TechWheel"
+      }
+      game:GetService("ReplicatedStorage").Network:FindFirstChild("Spinny Wheel: Request Spin"):InvokeServer(unpack(args))
+   end
 end)
 
-local ExampleColorPicker = Groupbox1:CreateColorPicker("Color", Color3.fromRGB(0, 0, 0), function(state)
-   print(state.R, state.G, state.B)
+tab:Toggle("Use VoidWheel",false, function(t)
+   _G.VoidWheel = t
+   while _G.VoidWheel do wait(.1)
+      local args = {
+      [1] = "VoidWheel"
+      }
+      game:GetService("ReplicatedStorage").Network:FindFirstChild("Spinny Wheel: Request Spin"):InvokeServer(unpack(args))
+   end
 end)
 
--- Groupbox 2
-local ExampleToggle2 = Groupbox2:CreateToggle("Example toggle", function(state)
-   print(state)
+------------------------------------------------------------------------------------------
+
+local sethumanoid = win:Tab("Set")
+
+sethumanoid:Textbox("Walk Speed",true, function(t)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = t
 end)
 
-local ExampleToggle3 = Groupbox2:CreateToggle("Example keybind", function(state)
-   print(state)
+sethumanoid:Textbox("Jump Power",true, function(t)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = t
 end)
 
-local ExampleButton2 = Groupbox2:CreateButton("Example Button", function()
-    print("Pressed")
+sethumanoid:Button("Inf Jump", function()
+  local InfiniteJumpEnabled = true
+    game:GetService("UserInputService").JumpRequest:connect(function()
+	if InfiniteJumpEnabled then
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+	end
+end)
 end)
 
-ExampleToggle3:CreateKeyBind()
 
-local ExampleSlider2 = Groupbox2:CreateSlider("Example slider", 0, 25, 0, function(value)
-   print(value)
+local teleport = win:Tab("Teleport")
+
+teleport:Button("Lower Player In Sever", function()
+      loadstring(game:HttpGet("https://github.com/fomm1802/101/raw/main/Lower%20Player%20In%20Sever.lua"))()
 end)
 
-local ExampleDropdown2 = Groupbox2:CreateDropdown("Example Dropdown", {"Option 1", "Option 2", "Option 3"}, function(state)
-   print(state)
+teleport:Button("Hopper Sever", function()
+  loadstring(game:HttpGet("https://github.com/fomm1802/101/raw/main/Hopper%20Server.lua"))()
 end)
 
-local ExampleColorPicker2 = Groupbox2:CreateColorPicker("Color", Color3.fromRGB(0, 0, 0), function(state)
-   print(state.R, state.G, state.B)
+local change = win:Tab("Change UI Color")
+
+change:Bind("Bind",Enum.KeyCode.K, function()
+print("Pressed!")
 end)
 
--- Groupbox 3
-local ExampleToggle4 = Groupbox3:CreateToggle("Example toggle", function(state)
-   print(state)
-end)
-
-local ExampleToggle5 = Groupbox3:CreateToggle("Example keybind", function(state)
-   print(state)
-end)
-
-local ExampleButton3 = Groupbox3:CreateButton("Example Button", function()
-    print("Pressed")
-end)
-
-ExampleToggle5:CreateKeyBind()
-
-local ExampleSlider3 = Groupbox3:CreateSlider("Example slider", 0, 25, 0, function(value)
-   print(value)
-end)
-
-local ExampleDropdown3 = Groupbox3:CreateDropdown("Example Dropdown", {"Option 1", "Option 2", "Option 3"}, function(state)
-   print(state)
-end)
-
-local ExampleColorPicker3 = Groupbox3:CreateColorPicker("Color", Color3.fromRGB(0, 0, 0), function(state)
-   print(state.R, state.G, state.B)
-end)
-
--- Groupbox 4
-local ExampleToggle5 = Groupbox4:CreateToggle("Example toggle", function(state)
-   print(state)
-end)
-
-local ExampleToggle6 = Groupbox4:CreateToggle("Example keybind", function(state)
-   print(state)
-end)
-
-local ExampleButton4 = Groupbox4:CreateButton("Example Button", function()
-    print("Pressed")
-end)
-
-ExampleToggle6:CreateKeyBind()
-
-local ExampleSlider4 = Groupbox4:CreateSlider("Example slider", 0, 25, 0, function(value)
-   print(value)
-end)
-
-local ExampleDropdown4 = Groupbox4:CreateDropdown("Example Dropdown", {"Option 1", "Option 2", "Option 3"}, function(state)
-   print(state)
-end)
-
-local ExampleColorPicker4 = Groupbox4:CreateColorPicker("Color", Color3.fromRGB(0, 0, 0), function(state)
-   print(state.R, state.G, state.B)
+change:Colorpicker("Change UI Color",Color3.fromRGB(44, 120, 224), function(t)
+lib:ChangePresetColor(Color3.fromRGB(t.R * 255, t.G * 255, t.B * 255))
 end)
