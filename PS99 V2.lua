1782fd3c5414f9c39606a2df2194569ca4f806f0
fomm1802 <106429671+fomm1802@users.noreply.github.com>
2024-07-12 20:24:32 +0700

    Update PS99 V2.lua

--- PS99 V2.lua
+++ PS99 V2.lua
@@ -1,32 +1,33 @@
 local library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ShaddowScripts/Main/main/Library"))()
 
-local Main = library:CreateWindow("Main","Crimson")
+local Main = library:CreateWindow("Main", "Crimson")
 
 local tab = Main:CreateTab("Cheats")
 local tab2 = Main:CreateTab("Misc")
 
-tab:CreateButton("Hi",function()
-print("clicked")
+tab:CreateButton("Hi", function()
+    print("clicked")
 end)
 
-tab:CreateToggle("Farm",function(a)
-print(a)
+tab:CreateToggle("Farm", function(a)
+    print(a)
 end)
 
-tab:CreateSlider("Wow",1,16,function(a)
-print(a)
+tab:CreateDropdown("Word", {"Sung", "Jin", "Woo"}, function(a)
+    print(a)
 end)
 
-tab:CreateCheckbox("Aimbot",function(a)
-print(a)
+tab2:CreateButton("Hello", function()
+    print("clicked")
 end)
 
-tab:CreateDropdown("Word",{"Sung","Jin","Woo"},function(a)
-print(a)
-end)
-
-tab2:CreateButton("Hello",function()
-print("clicked")
+-- Adding the new toggle for CrystalKey
+tab:CreateToggle("Auto Unlock CrystalKey", function(state)
+    _G.CrystalKey = state
+    while _G.CrystalKey do
+        wait(0.1)
+        game:GetService("ReplicatedStorage").Network.CrystalKey_Unlock:InvokeServer()
+    end
 end)
 
 tab:Show()
