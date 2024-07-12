4195c8326c14b074d3bfeaa3b60ade93c8966514
fomm1802 <106429671+fomm1802@users.noreply.github.com>
2024-07-12 20:33:10 +0700

    Update PS99 V2.lua

--- PS99 V2.lua
+++ PS99 V2.lua
@@ -1,49 +1,123 @@
-local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/StepBroFurious/Script/main/HydraHubUi.lua'))()
-local Window = UILib.new("Grand Piece Online", game.Players.LocalPlayer.UserId, "Buyer")
-local Category1 = Window:Category("Main", "http://www.roblox.com/asset/?id=8395621517")
-local SubButton1 = Category1:Button("Combat", "http://www.roblox.com/asset/?id=8395747586")
-local Section1 = SubButton1:Section("Section", "Left")
-Section1:Button({
-    Title = "Kill All",
-    ButtonName = "KILL!!",
-    Description = "kills everyone",
-    }, function(value)
-    print(value)
-end)
-Section1:Toggle({
-    Title = "Auto Farm Coins",
-    Description = "Optional Description here",
-    Default = false
-    }, function(value)
-    print(value)
-end)
-Section1:Slider({
-    Title = "Walkspeed",
-    Description = "",
-    Default = 16,
-    Min = 0,
-    Max = 120
-    }, function(value)
-    print(value)
-end)
-Section1:ColorPicker({
-    Title = "Colorpicker",
-    Description = "",
-    Default = Color3.new(255,0,0),
-    }, function(value)
-    print(value)
-end)
-Section1:Textbox({
-    Title = "Damage Multiplier",
-    Description = "",
-    Default = "",
-    }, function(value)
-    print(value)
-end)
-Section1:Keybind({
-    Title = "Kill All",
-    Description = "",
-    Default = Enum.KeyCode.P,
-    }, function(value)
-    print(value)
-end)
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Rain-Design/PPHUD/main/Library.lua'))()
+local Flags = Library.Flags
+
+local Window = Library:Window({
+   Text = "Baseplate"
+})
+
+local Tab = Window:Tab({
+   Text = "Aiming"
+})
+
+local Tab2 = Window:Tab({
+   Text = "Visual"
+})
+
+local Section = Tab:Section({
+   Text = "Aiming"
+})
+
+local Section2 = Tab:Section({
+   Text = "Anti-Aim"
+})
+
+local Section3 = Tab:Section({
+   Text = "Ragebot",
+   Side = "Right"
+})
+
+Section:Check({
+   Text = "Aimbot",
+   Flag = "Aimbot"
+})
+
+Section:Check({
+   Text = "Silent-Aim",
+   Callback = function(bool)
+       warn(bool)
+   end
+})
+
+Section:Dropdown({
+   Text = "Body Part",
+   List = {"Head", "Torso", "Random"},
+   Callback = function(opt)
+       warn(opt)
+   end
+})
+
+Section:Slider({
+   Text = "Hit Chance",
+   Minimum = 0,
+   Default = 60,
+   Maximum = 100,
+   Postfix = "%",
+   Callback = function(n)
+       warn(n)
+   end
+})
+
+Section:Button({
+   Text = "Spawn",
+   Callback = function()
+       warn("Settings Reseted.")
+   end
+})
+
+Section2:Check({
+   Text = "Spin"
+})
+
+Section2:Slider({
+   Text = "Pitch Offset",
+   Minimum = 100,
+   Default = 150,
+   Maximum = 500,
+   Callback = function(n)
+       warn(n)
+   end
+})
+
+Section2:Slider({
+   Text = "Yaw Offset",
+   Minimum = 100,
+   Default = 150,
+   Maximum = 500,
+   Callback = function(n)
+       warn(n)
+   end
+})
+
+Section2:Button({
+   Text = "Resolve Positions"
+})
+
+Section3:Check({
+   Text = "Auto-Wall",
+   Callback = function(bool)
+       warn(bool)
+   end
+})
+
+Section3:Check({
+   Text = "Trigger Bot"
+})
+
+Section3:Check({
+   Text = "Insta-Kill"
+})
+
+Section3:Dropdown({
+   Text = "Hitscan Directions",
+   List = {"Left", "Right", "Up", "Down", "All"},
+   Callback = function(opt)
+       warn(opt)
+   end
+})
+
+Section3:Label({
+   Text = "Status: Undetected",
+   Color = Color3.fromRGB(100, 190, 31)
+})
+
+Tab:Select()
