776bcbe1fb37742939ec588888005223b735a546
fomm1802 <106429671+fomm1802@users.noreply.github.com>
2024-07-12 18:41:54 +0700

    Update PS99 V2.lua

--- PS99 V2.lua
+++ PS99 V2.lua
@@ -1,3 +1,4 @@
+loadstring(game:Http("https://raw.githubusercontent.com/fomm1802/101/main/AFK.lua"))()
 local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt")()
 
 local win = lib:Window("PREVIEW",Color3.fromRGB(44, 120, 224),Enum.KeyCode.RightControl)
